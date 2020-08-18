dce9ddb591c260006be1a2f94461555847c0c65c
Marcus Arthur Klitzke Andriani <eng.marcusarthur@yahoo.com>
2020-08-17 21:48:43 -0300

    Comment included
    An exercise to create a file HTML with Python.

--- _exercises/_file_creation_with_py/createhtmleloopforpython.py
+++ _exercises/_file_creation_with_py/createhtmleloopforpython.py
@@ -1,4 +1,5 @@
 """
+An exercise to create a file HTML with Python.
 O código em Python cria um arquivo HTML que pode ser aberto direto no navegador usando a função(?)* open e escreve no arquivo usando o método (?)* write.
 Veja também a tabela de "conversion types" em https://docs.python.org/3/library/stdtypes.html#printf-style-string-formatting
 O artifício pode ser usado para criar arquivos com qualquer extensão, contanto que seja um arquivo escrito.
