399e81b77dcea6321c0526440e75bf5fac024797
Marcus Arthur Klitzke Andriani <marcusandriani@gmail.com>
2020-08-18 22:26:12 -0300

    Update string_elimination_char_repetition.py
    Transformed in a function.

--- _exercises/_python/string_elimination_char_repetition.py
+++ _exercises/_python/string_elimination_char_repetition.py
@@ -1,10 +1,25 @@
-print("Type something with character repetition and I will eliminate the repetition.", end="")
-sentence=input("->") #For example, type aaaaaaaaaaabbbbbbbbcccccccccc1111111122222233
-j=0
-sentence1=" "
-for i in sentence:
-    if sentence1[j]!=i:
-        sentence1=sentence1+i
-        j+=1
-sentence1=sentence1[1:] # to eliminate the inicial space setted on criation
-print(sentence1)
+#Eliminate repetitions on string; e.g. aaaaaaaaaa4444444++++ becomes a4+
+def elim_ch_repet(string_to_clean):
+    j=0
+    stringnr=" "
+    for i in string_to_clean:
+        if stringnr[j]!=i:
+            stringnr=stringnr+i
+            j+=1
+    stringnr=stringnr[1:] # to eliminate the inicial space setted on criation
+    return(stringnr)
+# End of function definition
+
+# Aplication:
+    # remove the triple quotation marks below to activate the code and verify the function
+#------------------------------------------------------------
+"""
+print("Type something with character repetition and I will eliminate the sequences of repetition.", end="")
+sentence=input("->") #For example, type >aaaaaaaaaaabbbbbbbbcccccccccc                 1111111122222233<
+
+newstring=elim_ch_repet(sentence) #call funciont sending the argument
+print(newstring)
+
+exit()
+"""
+#------------------------------------------------------------
