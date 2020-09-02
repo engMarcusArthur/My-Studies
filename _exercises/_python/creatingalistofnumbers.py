63889086a874d214f03eeba2e92eb9d979b237fc
Marcus Arthur Klitzke Andriani <marcusandriani@gmail.com>
2020-09-02 13:41:01 -0300

    movi para outro repositorio
    passei para o repositorio exclusivo de exercicios de python

--- _exercises/_python/creatingalistofnumbers.py
+++
@@ -1,6 +0,0 @@
-n=int(input("entre com numero"))
-p = []
-for i in range(n):
-    p.append(i+1)
-print("Updated list: ", p)
-print(p.index(1))
--- _exercises/_python/extraindoelementosdelistas.py
+++
@@ -1,9 +0,0 @@
-lista=["abcde","bfghi"]
-l1 = lista[0]
-l2 = lista[1]
-joined_list1 = [l1, l2]
-joined_list2 = [*l1, *l2]  # unpack both iterables in a list literal
-print(joined_list1)
-print(joined_list2)
-joined_list2.reverse()
-print(joined_list2)
